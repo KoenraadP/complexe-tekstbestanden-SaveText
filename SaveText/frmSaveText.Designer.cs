75bbc45f376fd0123d62db951cd08c718a522383
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-03-25 09:24:52 +0100

    Add UI

---
+++ SaveText/frmSaveText.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SaveText
+{
+    partial class frmSaveText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtStory = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(13, 13);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(267, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtStory
+            // 
+            this.txtStory.Location = new System.Drawing.Point(13, 40);
+            this.txtStory.Multiline = true;
+            this.txtStory.Name = "txtStory";
+            this.txtStory.Size = new System.Drawing.Size(267, 192);
+            this.txtStory.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(13, 238);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(149, 238);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(130, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // frmSaveText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 271);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtStory);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "frmSaveText";
+            this.Text = "SaveText";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtStory;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
--- SaveText/frmSaveText.cs
+++ SaveText/frmSaveText.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SaveText
 {
-    public partial class Form1 : Form
+    public partial class frmSaveText : Form
     {
-        public Form1()
+        public frmSaveText()
         {
             InitializeComponent();
         }
