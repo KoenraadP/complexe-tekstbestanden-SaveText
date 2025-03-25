b902f42861433291e3295623aebc848b743fc258
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-03-25 10:31:31 +0100

    Add finished Save

--- SaveText.Bll/TextBll.cs
+++ SaveText.Bll/TextBll.cs
@@ -6,21 +6,38 @@ using System.IO;
 namespace SaveText.Bll
 {
     // public want nodig in ander project
-    public class TextBll
+    // static want ik wil gewoon werken met TextBll.SaveText()
+    // en niet met new TextBll()
+    public static class TextBll
     {
         // directory: in welke map moet het bestand opgeslagen worden
         // title: onder welke naam moet het bestand opgeslagen worden
         // story: welke tekst komt effectief in het bestand
-        public void SaveText(string directory,
+        public static void SaveText(string directory,
             string title, string story)
         {
             // als de directory (map) nog niet bestaatn
             // dan error boodschap aanmaken en 'terug gooien'
-            if(!Directory.Exists(directory))
+            if (!Directory.Exists(directory))
             {
-                throw new DirectoryNotFoundException("Directory bestaat niet." +
+                throw new DirectoryNotFoundException("Map bestaat niet." +
                                                      " Wil je deze aanmaken?");
             }
+
+            // controleren of title en story
+            // niet leeg of null zijn
+            if (string.IsNullOrEmpty(title) ||
+               string.IsNullOrEmpty(story))
+            {
+                throw new ArgumentNullException("Title of story is leeg.");
+            }
+
+            // als alles OK is, pad samenstellen
+            // bijvoorbeeld: C:\stories\cinderella.txt
+            string path = directory + title + ".txt";
+
+            // bestand aanmaken en tekst wegschrijven
+            File.WriteAllText(path, story);
         }
     }
 }
--- SaveText/frmSaveText.cs
+++ SaveText/frmSaveText.cs
@@ -3,18 +3,66 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SaveText.Bll;
 
 namespace SaveText
 {
     public partial class frmSaveText : Form
     {
+
+        // globale variabele om de map op te slaan
+        string Directory = @"C:\stories\";
+
         public frmSaveText()
         {
             InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            // data uit mijn textboxes opslaan in variabelen
+            string title = txtTitle.Text;
+            string story = txtStory.Text;
+
+            // try catch om exceptions op te vangen
+            try
+            {
+                // TextBll SaveText methode uitvoeren
+                TextBll.SaveText(Directory, title, story); 
+                // boodschap tonen dat het gelukt is
+                MessageBox.Show(title + " opgeslagen.");
+            }
+            // probleem 'map bestaat niet' opvangen
+            catch (DirectoryNotFoundException dirEx)
+            {
+                // DialogResult vangt op welke knop
+                // we aangeklikt hebben bij de error message (yes or no)
+                DialogResult result =  MessageBox.Show(dirEx.Message,
+                    "Map bestaat niet", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                // als we op 'yes' klikken
+                if (result == DialogResult.Yes) {
+                    // map aanmaken
+                    // Specifiek System.IO er voor plaatsen omdat
+                    // er anders conflict is met mijn eigen Directory variabele
+                    System.IO.Directory.CreateDirectory(Directory);
+                }
+            }
+            // probleem 'niet goed ingevuld' opvangen
+            catch (ArgumentNullException argEx)
+            {
+                MessageBox.Show(argEx.Message);
+            }
+            // algemene fouten opvangen
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
