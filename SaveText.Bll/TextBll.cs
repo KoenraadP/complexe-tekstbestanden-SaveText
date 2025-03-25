dd588fe3a6f0e9e6bf959c39daaf288841a9307b
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-03-25 09:39:36 +0100

    Add SaveText method

--- SaveText.Bll/TextBll.cs
+++ SaveText.Bll/TextBll.cs
@@ -1,13 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// System.IO bevat allerlei classes en methodes
+// om met mappen en bestanden te werken
+using System.IO;
 
 namespace SaveText.Bll
 {
     // public want nodig in ander project
     public class TextBll
     {
+        // directory: in welke map moet het bestand opgeslagen worden
+        // title: onder welke naam moet het bestand opgeslagen worden
+        // story: welke tekst komt effectief in het bestand
+        public void SaveText(string directory,
+            string title, string story)
+        {
+            // als de directory (map) nog niet bestaatn
+            // dan error boodschap aanmaken en 'terug gooien'
+            if(!Directory.Exists(directory))
+            {
+                throw new DirectoryNotFoundException("Directory bestaat niet." +
+                                                     " Wil je deze aanmaken?");
+            }
+        }
     }
 }
